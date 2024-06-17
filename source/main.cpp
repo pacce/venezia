78142718952908f0d06da8949c8c59fc83e6d5dd
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2024-06-16 23:25:43 -0300

    feat: creates a simple tokenizing repl

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,32 @@
-#include <iostream>
+#include <boost/spirit/include/qi.hpp>
+#include <venezia/venezia.hpp>
+
+namespace qi = boost::spirit::qi;
+
+template <typename Iterator>
+venezia::Tokens
+scan(Iterator begin, Iterator end) {
+    venezia::lexer::lexersp<Iterator> grammar;
+    venezia::Tokens decoded;
+
+    if (qi::parse(begin, end, grammar, decoded)) {
+        return decoded; 
+    } else {
+        return {};
+    }
+}
 
 int
-main(int argc, char** argv) {
-    std::cout << "hello world" << std::endl;
+main(int, char**) {
+    while (true) {
+        std::cout << ">> ";
+
+        std::string line;
+        std::getline(std::cin, line);
+        venezia::Tokens tokens = scan(std::begin(line), std::end(line));
+        for (const venezia::Token& token : tokens) {
+            std::cout << token << std::endl;
+        }
+    }
     return 0;
 }
